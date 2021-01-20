6912e60e37883b39ba14db2800ee8618906939c9
ujasbatheja <71878150+ujasbatheja@users.noreply.github.com>
2021-01-20 18:35:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var hourHand, minuteHand, secondHand;
+
+function setup() {
+  createCanvas(800,400);
+
+  push();
+  rotate(scAngle);
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop();
+
+}
+
+function draw() {
+  background(255,255,255);  
+  
+  hr = hour();
+  mn = minute();
+  sc = second();
+
+  hourHand = createSprite(400,200,300,10);
+  minuteHand = createSprite(400,200,200,10);
+  secondHand = createSprite(400,200,100,10);
+
+  angleMode(10);
+  hrAngle = map(hr,0,60,0,360);
+  mnAngle = map(mn,0,60,0,360);
+  scAngle = map(sc,0,60,0,360);
+
+  arc(50, 55, 50, 50, 0, HALF_PI);
+  noFill();
+  arc(50, 55, 60, 60, HALF_PI, PI);
+  arc(50, 55, 70, 70, PI, PI + QUARTER_PI);
+  arc(50, 55, 80, 80, PI + QUARTER_PI, TWO_PI);
+
+  drawSprites();
+}
